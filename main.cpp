9fa5acea711c9a4ae91d0cdc645214ce4294f5fd
igordpo <igor.d.oliveira@edu.senai.br>
2026-05-20 10:31:33 -0300

    feat: adiciona notas e calculo de média

--- main.cpp
+++ main.cpp
@@ -5,10 +5,18 @@ using namespace std;
 
 int main(){
 
+
+
+                        //VARIAVEIS
  string nomes[20];
  int qtdAlunos;
+ float notas[20][5];
+ float media[20];
+ int qtdDisciplinas;
 
 
+
+                        //processamento
  cout << "======SISTEMA DE NOTAS v4.0======" << endl;
 
  do{
@@ -28,7 +36,25 @@ int main(){
  }
 
 
+ do {
+    cout << "\nQuantidade de disciplinas (1 a 5): " << endl;
+    cin >> qtdDisciplinas;
+ }while( qtdDisciplinas < 1 || qtdDisciplinas > 5);
 
+ for (int i = 0; i < qtdAlunos; i++){
+    cout << "\nNotas de " << nomes[i] << ": " << endl;
+    float soma = 0;
+     for (int j = 0; j < qtdDisciplinas; j++){
+        do{
+            cout << "Disciplina " << j + 1 << "(1 a 5): ";
+            cin >> notas[i][j];
+        }while(notas[i][j] < 1 || notas[i][j] > 10);
+        soma += notas[i][j];
+     } 
+     media[i] = soma / qtdDisciplinas;
+ }
+
+            //SAIDA
  cout << "\nAlunos cadastrados: " << endl;
  for (int i = 0; i < qtdAlunos; i++){
     cout << "  " << i + 1 << ". " << nomes[i] << endl;
