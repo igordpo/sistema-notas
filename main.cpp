9252863119ef9f9bdb2b017ef942bc2acb7e43ed
igordpo <igor.d.oliveira@edu.senai.br>
2026-05-20 11:01:15 -0300

    feat: adiciona  classificaçao de relatorio

--- main.cpp
+++ main.cpp
@@ -2,63 +2,85 @@
 #include <string>
 using namespace std;
 
+int main()
+{
 
-int main(){
+    // VARIAVEIS
+    string nomes[20];
+    int qtdAlunos;
+    float notas[20][5];
+    float media[20];
+    int qtdDisciplinas;
 
+    // processamento
+    cout << "======SISTEMA DE NOTAS v4.0======" << endl;
 
+    do
+    {
+        cout << "Quantidade de alunos (1 a 20): ";
+        cin >> qtdAlunos;
+    } while (qtdAlunos < 1 || qtdAlunos > 20);
 
-                        //VARIAVEIS
- string nomes[20];
- int qtdAlunos;
- float notas[20][5];
- float media[20];
- int qtdDisciplinas;
+    cin.ignore();
 
+    for (int i = 0; i < qtdAlunos; i++)
+    {
+        cout << "Nome do Aluno " << i + 1 << ": ";
+        getline(cin, nomes[i]);
+    }
 
+    do
+    {
+        cout << "\nQuantidade de disciplinas (1 a 5): " << endl;
+        cin >> qtdDisciplinas;
+    } while (qtdDisciplinas < 1 || qtdDisciplinas > 5);
 
-                        //processamento
- cout << "======SISTEMA DE NOTAS v4.0======" << endl;
+    for (int i = 0; i < qtdAlunos; i++)
+    {
+        cout << "\nNotas de " << nomes[i] << ": " << endl;
+        float soma = 0;
+        for (int j = 0; j < qtdDisciplinas; j++)
+        {
+            do
+            {
+                cout << "Disciplina " << j + 1 << "(1 a 5): ";
+                cin >> notas[i][j];
+            } while (notas[i][j] < 1 || notas[i][j] > 10);
+            soma += notas[i][j];
+        }
+        media[i] = soma / qtdDisciplinas;
+    }
 
- do{
-    cout << "Quantidade de alunos (1 a 20): ";
-    cin >> qtdAlunos;
- } while (qtdAlunos < 1 || qtdAlunos > 20);
+    // SAIDA
+    cout << "\nAlunos cadastrados: " << endl;
+    for (int i = 0; i < qtdAlunos; i++)
+    {
+        cout << "  " << i + 1 << ". " << nomes[i] << endl;
+    }
 
+    cout << "\n====RELATORIO====" << endl;
+    int aprovados = 0, recuperaçao = 0, reprovados = 0;
 
+    for (int i = 0; i < qtdAlunos; i++)
+    {
+        cout << nomes[i] << "- media: " << media[i] << " -";
+        if (media[i] >= 7)
+        {
+            cout << "Aprovado" << endl;
+            aprovados++;
+        }
+        else if (media[i] >= 5)
+        {
+            cout << "Recuperaçao" << endl;
+            recuperaçao++;
+        }
+        else
+        {
+            cout << "Reprovado" << endl;
+            reprovados++;
+        }
+    }
+    cout << "\nResumo: " << aprovados << "Aprovados, " << recuperaçao << " em recuperaçao, " << reprovados << "Reprovados." << endl;
 
- cin.ignore();
-
-
-
- for (int i = 0; i < qtdAlunos; i++){
-    cout << "Nome do Aluno " << i + 1 << ": ";
-    getline(cin,nomes[i]);
- }
-
-
- do {
-    cout << "\nQuantidade de disciplinas (1 a 5): " << endl;
-    cin >> qtdDisciplinas;
- }while( qtdDisciplinas < 1 || qtdDisciplinas > 5);
-
- for (int i = 0; i < qtdAlunos; i++){
-    cout << "\nNotas de " << nomes[i] << ": " << endl;
-    float soma = 0;
-     for (int j = 0; j < qtdDisciplinas; j++){
-        do{
-            cout << "Disciplina " << j + 1 << "(1 a 5): ";
-            cin >> notas[i][j];
-        }while(notas[i][j] < 1 || notas[i][j] > 10);
-        soma += notas[i][j];
-     } 
-     media[i] = soma / qtdDisciplinas;
- }
-
-            //SAIDA
- cout << "\nAlunos cadastrados: " << endl;
- for (int i = 0; i < qtdAlunos; i++){
-    cout << "  " << i + 1 << ". " << nomes[i] << endl;
- }
-
- return 0;    
+    return 0;
 }
